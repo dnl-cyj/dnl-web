9a2c51fce70f8e7c31af88e115eda3e69a348e33
cyj <ying511307543@163.com>
2024-10-08 11:25:17 +0800

    配置src路径别名

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+const src = path.resolve(__dirname, "src")
+console.log("src",src)
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({mode}) => {
+    return {
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "src")
+            }
+        },
+        plugins: [vue()],
+    }
 })
