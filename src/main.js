6858d9dd3dd13cec882c27297dad091a4caf0316
ying5 <ying511307543@163.com>
2024-11-22 17:22:01 +0800

    全局注册路由实例

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import pinia from "@/stores";
+import router from "@/routers";
 import './style.css'
 import 'uno.css'
 import App from './App.vue'
 
 createApp(App)
     .use(pinia)
+    .use(router)
     .mount('#app')
